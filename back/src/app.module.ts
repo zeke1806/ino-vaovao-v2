7c025a32b8b360ba5782748d795e46d886b5b1eb
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-08-31 09:51:12 +0300

    feat(back): setup typeorm + migration

---
+++ back/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConnectionOptions } from 'typeorm';
+
+import configs from './configs';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [configs],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return configService.get<ConnectionOptions>('database');
+      },
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
