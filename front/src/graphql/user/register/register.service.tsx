3ec2d463af05e5ccb6377c3b1d04324fd6336710
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-09-07 16:32:23 +0300

    feat(mobile): mock react navigation hook usenavigation in test register.service.spec

--- front/src/graphql/user/register/register.service.tsx
+++ front/src/graphql/user/register/register.service.tsx
@@ -1,83 +1,10 @@
-import * as React from 'react';
 import { REGISTER, RegisterData } from './register.gql';
+import { UseRegisterForm, useRegisterForm } from './useRegisterForm';
 import { AuthenticationNavigatorParamList } from '../../../navigations/AuthenticationNavigator';
 import { MutationRegisterArgs } from '../../types';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { useImmer } from 'use-immer';
 import { useMutation } from '@apollo/client';
 import { useNavigation } from '@react-navigation/native';
-
-export type Keys = 'username' | 'password' | 'validatePassword';
-export type RegisterForm = {
-  validatePassword: string;
-  password: string;
-  username: string;
-};
-export interface UseRegisterForm {
-  handleChangeFormRegister: (key: Keys, value: string) => void;
-  resetForm: () => void;
-  registerForm: RegisterForm;
-  errorFormRegister: boolean;
-  isFormOk: () => boolean;
-}
-
-export const useRegisterForm = (): UseRegisterForm => {
-  const [errorFormRegister, setError] = React.useState(false);
-  const [validatePassword, setValidatePassword] = React.useState('');
-  const [variables, setVariables] = useImmer<MutationRegisterArgs>({
-    registerInput: {
-      username: '',
-      password: '',
-    },
-  });
-
-  const handleChangeFormRegister = (key: Keys, value: string): void => {
-    if (key === 'username' || key === 'password') {
-      setVariables((draft) => {
-        draft.registerInput[key] = value;
-      });
-    } else if (key === 'validatePassword') {
-      setValidatePassword(value);
-    }
-  };
-
-  const resetForm = (): void => {
-    setError(false);
-    setValidatePassword('');
-    setVariables((draft) => {
-      draft.registerInput.username = '';
-      draft.registerInput.password = '';
-    });
-  };
-
-  const isFormOk = (): boolean => {
-    if (
-      variables.registerInput.password === '' ||
-      variables.registerInput.username === '' ||
-      validatePassword === '' ||
-      variables.registerInput.password !== validatePassword
-    ) {
-      // error
-      setError(true);
-      return false;
-    } else {
-      return true;
-    }
-  };
-
-  const registerForm: RegisterForm = {
-    ...variables.registerInput,
-    validatePassword,
-  };
-
-  return {
-    handleChangeFormRegister,
-    resetForm,
-    registerForm,
-    errorFormRegister,
-    isFormOk,
-  };
-};
 
 export type UseRegister = {
   submitRegister: () => void;
--- front/src/graphql/user/register/register.spec.tsx
+++ front/src/graphql/user/register/register.spec.tsx
@@ -5,16 +5,15 @@ import {
   act,
   renderHook,
 } from '@testing-library/react-hooks';
-import {
-  Keys,
-  RegisterForm,
-  UseRegisterForm,
-  useRegister,
-  useRegisterForm,
-} from './register.service';
+import { Keys, RegisterForm } from './useRegisterForm';
 import { MockedProvider, MockedResponse } from '@apollo/react-testing';
 import { REGISTER } from './register.gql';
 import { User } from '../../types';
+import { useRegister } from './register.service';
+
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: jest.fn(),
+}));
 
 // :: DATA ::
 const user: User = {
@@ -108,81 +107,6 @@ describe('useRegister custom hook', () => {
       expect(result.current.loadingRegister).toBeFalsy();
       // eslint-disable-next-line no-console
       expect(console.log).toHaveBeenCalledWith('register complete');
-    });
-  });
-});
-
-describe('useRegisterForm custom hook', () => {
-  const changeFormInput = (
-    result: HookResult<UseRegisterForm>,
-    username: string,
-    password: string,
-    validatePassword: string,
-  ): void => {
-    act(() => {
-      result.current.handleChangeFormRegister('username', username);
-      result.current.handleChangeFormRegister('password', password);
-      result.current.handleChangeFormRegister(
-        'validatePassword',
-        validatePassword,
-      );
-    });
-
-    expect(result.current.registerForm).toEqual({
-      username,
-      password,
-      validatePassword,
-    });
-  };
-
-  const expectRegisterFormToBeEmpty = (
-    result: HookResult<UseRegisterForm>,
-  ): void => {
-    expect(result.current.registerForm).toEqual({
-      username: '',
-      password: '',
-      validatePassword: '',
-    });
-  };
-
-  describe('handleChangeFormRegister', () => {
-    it('should change form input', () => {
-      const { result } = renderHook(() => useRegisterForm());
-      expectRegisterFormToBeEmpty(result);
-      changeFormInput(result, 'user', 'userpass', 'userpass');
-    });
-  });
-
-  describe('resetForm', () => {
-    it('should rest form', () => {
-      const { result } = renderHook(() => useRegisterForm());
-
-      changeFormInput(result, 'user', 'userpass', 'userpass');
-
-      act(() => {
-        result.current.resetForm();
-      });
-      expectRegisterFormToBeEmpty(result);
-      expect(result.current.errorFormRegister).toBeFalsy();
-    });
-  });
-
-  describe('isFormOk', () => {
-    it('should return true', () => {
-      const { result } = renderHook(() => useRegisterForm());
-      changeFormInput(result, 'user', 'userpass', 'userpass');
-      expect(result.current.isFormOk()).toBeTruthy();
-    });
-
-    it('should return false and errorFormRegister should be true', () => {
-      const { result } = renderHook(() => useRegisterForm());
-      changeFormInput(result, 'user', 'userpass', 'userp');
-      let isFormOk = true;
-      act(() => {
-        isFormOk = result.current.isFormOk();
-      });
-      expect(isFormOk).toBeFalsy();
-      expect(result.current.errorFormRegister).toBeTruthy();
     });
   });
 });
