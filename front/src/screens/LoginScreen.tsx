ecf8a2218ee7c7feaaeae6c9ff9ce4b829f27a22
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-09-08 11:43:01 +0300

    feat(mobile): login screen

--- front/src/screens/LoginScreen.tsx
+++ front/src/screens/LoginScreen.tsx
@@ -1,19 +1,33 @@
 import * as React from 'react';
-import { Container, Text } from 'native-base';
-import { StyleSheet } from 'react-native';
+import { Container, H2 } from 'native-base';
+import { ScrollView, StyleSheet } from 'react-native';
+import { screenHeight, screenWidth } from '../utils/Styles';
+import Constants from 'expo-constants';
+import LoginForm from '../components/login/LoginForm';
 
 const LoginScreen: React.FC = () => {
   return (
-    <Container style={styles.container}>
-      <Text>loginscreen</Text>
-    </Container>
+    <ScrollView>
+      <Container style={styles.container}>
+        <H2 style={styles.title}>Connexion</H2>
+        <LoginForm />
+      </Container>
+    </ScrollView>
   );
 };
 
+const spaceY = (screenHeight * 7) / 100;
+const spaceX = (screenWidth * 10) / 100;
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
+    padding: spaceX,
+    paddingTop: Constants.statusBarHeight,
+  },
+
+  title: {
+    marginBottom: spaceY,
   },
 });
 
