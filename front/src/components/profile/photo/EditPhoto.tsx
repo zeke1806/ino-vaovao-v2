92e713f88d41a39fed1c33b5d932d4443250d0d5
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-10-07 12:58:53 +0300

    feat(front): upload photo profile in progress

--- front/src/components/profile/photo/EditPhoto.tsx
+++ front/src/components/profile/photo/EditPhoto.tsx
@@ -1,9 +1,25 @@
+import * as ImagePicker from 'expo-image-picker';
 import * as React from 'react';
 import { Button, Thumbnail } from 'native-base';
 import { PENCIL } from '../../../utils/Icons';
 import { globalStyles } from '../../../styles/global';
+import { usePermissionPickImage } from '../../../utils/permissionPickImage';
 
 const EditPhoto: React.FC = () => {
+  usePermissionPickImage();
+  const pickImage = async (): Promise<void> => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      console.log(result.uri);
+    }
+  };
+
   return (
     <Button
       transparent
@@ -12,6 +28,7 @@ const EditPhoto: React.FC = () => {
         bottom: 0,
         left: '30%',
       }}
+      onPress={pickImage}
     >
       <Thumbnail
         source={PENCIL}
