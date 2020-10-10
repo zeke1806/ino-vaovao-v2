2408424e944d6e4318eac0b94f2d9675b75f4d76
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-10-10 19:56:27 +0300

    feat(front): background color navigation theme

--- front/src/navigations/RootStackNavigator.tsx
+++ front/src/navigations/RootStackNavigator.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
+import {
+  DefaultTheme,
+  NavigationContainer,
+  Theme,
+} from '@react-navigation/native';
 import AuthenticationNavigator from './AuthenticationNavigator';
 import MainNavigator from './MainNavigator';
-import { NavigationContainer } from '@react-navigation/native';
 import PresentationNavigator from './PresentationNavigator';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useSessionState } from '../providers/session/session.consumer';
+
+const MyTheme: Theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#fff',
+  },
+};
 
 type RootNavigatorParamList = {
   Presentation: undefined;
@@ -18,7 +30,7 @@ function RootNavigator(): React.ReactElement {
   const sessionState = useSessionState();
 
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={MyTheme}>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {sessionState.firstUsage && (
           <Stack.Screen name="Presentation" component={PresentationNavigator} />
