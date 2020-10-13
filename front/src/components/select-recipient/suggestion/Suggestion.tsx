0f850f83c4210723dc0e2d133b34e51cc6bc74f8
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-10-13 08:28:08 +0300

    feat(front): selected recipient screen integration ok

--- front/src/components/select-recipient/suggestion/Suggestion.tsx
+++ front/src/components/select-recipient/suggestion/Suggestion.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { FlatList, ListRenderItem } from 'react-native';
 import Item from './Item';
-import { globalStyles } from '../../../styles/global';
 
 const Suggestion: React.FC = () => {
   const renderItem: ListRenderItem<unknown> | null | undefined = () => <Item />;
