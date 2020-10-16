4782da174408c1d4908b5696a05cc71b1a39b077
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-10-16 11:07:14 +0300

    feat(back): send, accepte friend request

--- back/src/user/user.test-module.ts
+++ back/src/user/user.test-module.ts
@@ -4,12 +4,19 @@ import { UserService } from './user.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UtilsModule } from '../utils/utils.module';
+import { ConfigModule } from '@nestjs/config';
+import configs from '../configs';
 
-class UserRepositoryFake {}
+class UserRepositoryFake { }
 
 export async function userTestModule(): Promise<TestingModule> {
   return Test.createTestingModule({
-    imports: [UtilsModule],
+    imports: [
+      ConfigModule.forRoot({
+        load: [configs],
+      }),
+      UtilsModule
+    ],
     providers: [
       ...UserResolvers,
       UserService,
