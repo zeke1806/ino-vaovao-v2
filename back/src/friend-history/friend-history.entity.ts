2d5454aa2879fc877e91419d1e611b89541aa64b
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-10-20 08:23:49 +0300

    refactor(back): sync db

--- back/src/friend-history/friend-history.entity.ts
+++ back/src/friend-history/friend-history.entity.ts
@@ -10,14 +10,14 @@ export class FriendHistory {
   @JoinColumn({ name: 'user' })
   @Field()
   user: User;
-  @RelationId((friend: FriendHistory) => friend.user)
+  @RelationId((FriendHistory: FriendHistory) => FriendHistory.user)
   userId: number;
 
   @ManyToOne(() => User, { primary: true })
   @JoinColumn({ name: 'friend' })
   @Field()
   friend: User;
-  @RelationId((friend: FriendHistory) => friend.friend)
+  @RelationId((friendHistory: FriendHistory) => friendHistory.friend)
   friendId: number;
 
   @Field()
