6ae6532c3451d7e811470ed9f158c33bde12517c
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-10-27 19:07:15 +0300

    feat(front): subscription send friend request event

--- front/src/api/friend-history/send-friend-request/subscription.service.ts
+++ front/src/api/friend-history/send-friend-request/subscription.service.ts
@@ -3,6 +3,10 @@ import {
   FRIEND_REQUESTS,
   FriendRequestsData,
 } from '../../user/friend-request/gql';
+import {
+  FRIEND_SUGGESTION,
+  FriendSuggestionData,
+} from '../../user/friend-suggestion/friendSuggestion.gql';
 import { FriendHistory, User } from '../../types';
 import { ME, MeData } from '../../user/me/me.gql';
 import {
@@ -14,33 +18,59 @@ import produce from 'immer';
 
 function updateFriendRequest(
   apollo: ApolloClient<Record<string, any>>,
-  me: User,
   fh: FriendHistory,
 ): void {
   const prev =
-    apollo.readQuery<FriendRequestsData>({ query: FRIEND_REQUESTS }) ||
+    apollo.cache.readQuery<FriendRequestsData>({ query: FRIEND_REQUESTS }) ||
     ({
       friendRequests: [],
     } as FriendRequestsData);
 
-  if (me.id === fh.friend.id) {
-    apollo.writeQuery<FriendRequestsData>({
-      query: FRIEND_REQUESTS,
-      data: produce(prev, (draft) => {
-        draft.friendRequests.unshift(fh.user);
-      }),
-    });
-  }
+  apollo.cache.writeQuery<FriendRequestsData>({
+    query: FRIEND_REQUESTS,
+    data: produce(prev, (draft) => {
+      draft.friendRequests.unshift(fh.user);
+    }),
+  });
+}
+
+function updateFriendSuggestion(
+  apollo: ApolloClient<Record<string, any>>,
+  fh: FriendHistory,
+): void {
+  const prev =
+    apollo.cache.readQuery<FriendSuggestionData>({
+      query: FRIEND_SUGGESTION,
+    }) ||
+    ({
+      friendSuggestion: [],
+    } as FriendSuggestionData);
+
+  apollo.cache.writeQuery<FriendSuggestionData>({
+    query: FRIEND_SUGGESTION,
+    data: produce(prev, (draft) => {
+      draft.friendSuggestion.length &&
+        draft.friendSuggestion.splice(
+          draft.friendSuggestion.map((sugg) => sugg.id).indexOf(fh.user.id),
+          1,
+        );
+    }),
+  });
 }
 
 export const useSendFriendRequestEvent = (): void => {
   const apollo = useApolloClient() as ApolloClient<Record<string, any>>;
-  const meData = apollo.readQuery<MeData>({ query: ME });
 
   useSubscription<SendFriendRequestEventData>(SEND_FRIEND_REQUEST_EVENT, {
     onSubscriptionData({ subscriptionData: { data } }) {
-      if (data && meData) {
-        updateFriendRequest(apollo, meData.me, data.sendFriendRequestEvent);
+      const meData = apollo.readQuery<MeData>({ query: ME });
+      if (
+        data &&
+        meData &&
+        meData.me.id === data.sendFriendRequestEvent.friend.id
+      ) {
+        updateFriendRequest(apollo, data.sendFriendRequestEvent);
+        updateFriendSuggestion(apollo, data.sendFriendRequestEvent);
       }
     },
   });
