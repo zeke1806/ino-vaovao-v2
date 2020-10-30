030ef79ca8cb7eafa8a5a0f1da3ffd2ccd4f37ef
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-10-30 19:29:12 +0300

    feat(back): paginate message + create message by discussion qb

--- back/src/message/message.service.ts
+++ back/src/message/message.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, SelectQueryBuilder } from 'typeorm';
+import {
+  paginate,
+  Pagination,
+  IPaginationOptions,
+} from 'nestjs-typeorm-paginate';
 
 import { Message } from './message.entity';
 
@@ -11,20 +16,19 @@ export class MessageService {
     private messageRepository: Repository<Message>,
   ) {}
 
+  paginateMessage(
+    qb: SelectQueryBuilder<Message>,
+    options: IPaginationOptions,
+  ): Promise<Pagination<Message>> {
+    return paginate<Message>(qb, options);
+  }
+
   getMessageById(messageId: number): Promise<Message> {
     return this.messageRepository.findOne(messageId);
   }
 
   saveMessage(message: Message): Promise<Message> {
     return this.messageRepository.save(message);
-  }
-
-  getMessageByDiscussion(discussionId: number): Promise<Message[]> {
-    return this.messageRepository.find({
-      where: {
-        discussionId,
-      },
-    });
   }
 
   getLastDiscussionMessage(
@@ -37,5 +41,13 @@ export class MessageService {
       .select('MAX(message.id)', 'lastMessageId')
       .where(`message.discussion = ${discussionId}`)
       .getRawOne<{ lastMessageId: number }>();
+  }
+
+  createMessageByDiscussionQB(
+    discussionId: number,
+  ): SelectQueryBuilder<Message> {
+    return this.messageRepository
+      .createQueryBuilder('message')
+      .where(`message.discussion = ${discussionId}`);
   }
 }
