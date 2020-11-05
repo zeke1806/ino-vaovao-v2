dd82c970b0452090362acfc409cabc52d8b5afcc
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-11-05 15:06:35 +0300

    fix(bug): key args messages query

--- front/src/api/cache.ts
+++ front/src/api/cache.ts
@@ -1,3 +1,13 @@
 import { InMemoryCache } from '@apollo/client';
 
-export const cache = new InMemoryCache({});
+export const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        messages: {
+          keyArgs: ['discussionId'],
+        },
+      },
+    },
+  },
+});
