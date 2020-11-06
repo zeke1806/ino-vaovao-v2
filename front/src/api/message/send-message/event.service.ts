94c0e3958a1600fc05e3f950666af74e1427b221
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-11-06 19:34:34 +0300

    feat(front): notification sound

--- front/src/api/message/send-message/event.service.ts
+++ front/src/api/message/send-message/event.service.ts
@@ -6,14 +6,51 @@ import {
   SendMessageEventVariables,
 } from './event.gql';
 
+import { Audio } from 'expo-av';
 import { useMe } from '../../user/me/me.service';
 import { useSubscription } from '@apollo/client';
 
-type Return = SendMessageEventData | undefined;
+export const useAudio = (): (() => void) => {
+  const [sound, setSound] = React.useState<Audio.Sound | null>(null);
 
+  React.useEffect(() => {
+    Audio.setAudioModeAsync({
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
+      shouldDuckAndroid: true,
+      staysActiveInBackground: true,
+      playThroughEarpieceAndroid: true,
+    }).then(() => {
+      setSound(new Audio.Sound());
+    });
+  }, []);
+
+  React.useEffect(() => {
+    if (sound) {
+      const status = {
+        shouldPlay: false,
+      };
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      sound.loadAsync(require('../../../../assets/c.mp3'), status, false);
+    }
+  }, [sound]);
+
+  const playSound = (): void => {
+    if (sound) {
+      sound.playAsync().then(() => {
+        if (sound) {
+          sound.setPositionAsync(0);
+        }
+      });
+    }
+  };
+
+  return playSound;
+};
+
+type UseSendMessageEventReturn = SendMessageEventData | undefined;
 export const useSendMessageEvent = (
   variables: SendMessageEventVariables,
-): Return => {
+): UseSendMessageEventReturn => {
   const { data } = useSubscription<
     SendMessageEventData,
     SendMessageEventVariables
@@ -25,13 +62,18 @@ export const useSendMessageEvent = (
 };
 
 export const useNotificationSound = (): void => {
+  const playSound = useAudio();
   const { meData } = useMe();
   const varsId = meData ? meData.me.id : 0;
   const data = useSendMessageEvent({ clientId: varsId, userId: varsId });
 
   React.useEffect(() => {
-    if (data) {
-      console.log('play sound');
+    if (data && data.sendMessageEvent.lastMessage && meData) {
+      if (
+        data.sendMessageEvent.lastMessage.message.sender.id !== meData.me.id
+      ) {
+        playSound();
+      }
     }
   }, [data]);
 };
