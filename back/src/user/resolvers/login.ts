0021dfa0255e53c6a9bea4107a5b6b01e5415af9
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-12-22 13:53:11 +0300

    login

--- back/src/user/resolvers/login.ts
+++ back/src/user/resolvers/login.ts
@@ -2,15 +2,17 @@ import { MutationLoginArgs, Resolver, Token } from "../../types";
 import { UserService } from "../user.service";
 import bcrypt from "bcrypt";
 import { setTokens } from "../../libs/json-web-token";
+import { ApolloError } from "apollo-server-express";
 
 type T = Resolver<Token | null, {}, {}, MutationLoginArgs>;
 
 export const login: T = async (_, { username, password }) => {
   const userService = new UserService();
   const user = await userService.getByUsername(username);
+  const error = new ApolloError('connection info error');
 
-  if (!user) return null;
-  if (!(await bcrypt.compare(password, user.password))) return null;
+  if (!user) throw error;
+  if (!(await bcrypt.compare(password, user.password))) throw error;
 
   return {
     token: setTokens(user),
